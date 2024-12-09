f8216dfe269c2dfa674ef52f1e2d7e03eec3f220
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-12-09 13:29:59 +0530

    app js

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Reducer from './components/Reducer'
+import CartReducer from './components/CartReducer'
+import ThemeToggle from './components/ThemeToggle'
+
+const App = () => {
+  return (
+    <div>
+      {/* <Reducer /> */}
+      <CartReducer />
+      {/* <ThemeToggle /> */}
+    </div>
+  )
+}
+
+export default App
